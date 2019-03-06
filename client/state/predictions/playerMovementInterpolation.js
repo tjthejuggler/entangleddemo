accbe78d1305e006f76c266bfbc71c98a6ec487c
tjthejuggler <Github183630>
2019-03-06 13:06:02 -0600

    working on f and g keys

--- client/state/predictions/playerMovementInterpolation.js
+++ client/state/predictions/playerMovementInterpolation.js
@@ -21,8 +21,8 @@ const playerMovementInterpolation = otherPlayers => {
       player.speedText.y += (player.speedText.target_y - player.speedText.y) * 0.30
 
       //TJ modified this line to make myVariable show (hopefully)
-      //player.updatePlayerStatusText('speed2', player.speedText.x, player.speedText.y, player.speedText)
-      player.updatePlayerStatusText('speed2', player.speedText.x, player.speedText.y, player.myVariable)
+      player.updatePlayerStatusText('speed2', player.speedText.x, player.speedText.y, player.speedText)
+      //player.updatePlayerStatusText('speed2', player.speedText.x, player.speedText.y, player.myVariable)
     }
   }
 }
