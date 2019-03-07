cb5ba999b614c8a5a4e319239d29664fd12024cd
tjthejuggler <Github183630>
2019-03-07 17:44:43 -0600

    trying to get myVariable to be displayed for all players

--- client/state/player/index.js
+++ client/state/player/index.js
@@ -111,7 +111,7 @@ export default function (x, y, game, socket) {
     updatePlayerStatusText (status, x, y, text, mySentVariable) {
       // Capitalize the status text
       //const capitalizedStatus = status[0].toUpperCase() + status.substring(1)
-      const capitalizedStatus = mySentVariable.toString()
+      const capitalizedStatus = this.myVariable.toString()
       let newText = ''
       // Set the speed text to either 0 or the current speed
       this[status] < 0 ? this.newText = 0 : this.newText = this[status]
