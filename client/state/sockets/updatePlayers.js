46dd78933e3949aec0cdaca7f1c0e5dce8a6e910
tjthejuggler <Github183630>
2019-03-10 13:56:00 -0500

    trying to get myVariable to be displayed for all players

--- client/state/sockets/updatePlayers.js
+++ client/state/sockets/updatePlayers.js
@@ -11,7 +11,7 @@ const updatePlayers = (socket, otherPlayers, game) => {
       // We make sure that we won't create a second instance of it
 
       if (otherPlayers[index] === undefined && index !== socket.id) {
-        alert("hi1");
+        alert(otherPlayers.toString());
         const newPlayer = player(data.x, data.y, game)
         newPlayer.playerName = createText(game, newPlayer)
         newPlayer.speedText = createText(game, newPlayer)
