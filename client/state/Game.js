1fbbba332a44d99ee799ac6b1b40137eaa5f8cec
tjthejuggler <Github183630>
2019-03-11 18:11:10 -0500

    trying to get myVariable to be displayed for all players

--- client/state/Game.js
+++ client/state/Game.js
@@ -86,7 +86,7 @@ class Game extends Phaser.State {
 
     //alert("test");
     //alert(countPlayers(otherPlayers));
-    alert(otherPlayers.size().toString());
+    alert(otherPlayers);
 
     // Scale game to fit the entire window
     this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
