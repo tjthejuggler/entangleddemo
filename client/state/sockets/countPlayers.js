2e02b6970f8009ad2c3d8a90cae02c9067cde1a4
tjthejuggler <Github183630>
2019-03-11 17:13:55 -0500

    trying to get myVariable to be displayed for all players

--- client/state/sockets/countPlayers.js
+++ client/state/sockets/countPlayers.js
@@ -1,11 +1,11 @@
-function countPlayers (){
-//const countPlayers(otherPlayers) {
-  // var counter = 0
-  // for (let id in otherPlayers) {
-  //   counter = counter+1
-  // }
-  //return counter.toString()
-  return "myTest"
+function countPlayers (otherPlayers){
+
+  var counter = 0
+  for (let id in otherPlayers) {
+    counter = counter+1
+  }
+  return counter.toString()
+  //return "myTest"
 }
 
 export default countPlayers
