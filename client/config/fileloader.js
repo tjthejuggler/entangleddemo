b15bcdf23c7cf3919365358facaf3123685e4ce7
tjthejuggler <Github183630>
2019-03-11 20:08:19 -0500

    switching car.png with vaylaHead.png

--- client/config/fileloader.js
+++ client/config/fileloader.js
@@ -4,7 +4,7 @@ const fileLoader = game => {
   game.load.crossOrigin = 'Anonymous'
   game.stage.backgroundColor = '#1E1E1E'
   game.load.image('asphalt', `${ASSETS_URL}/sprites/asphalt/asphalt.png`)
-  game.load.image('car', `${ASSETS_URL}/sprites/car/car.png`)
+  game.load.image('car', `${ASSETS_URL}/sprites/car/vaylaHead.png`)
 }
 
 export default fileLoader
