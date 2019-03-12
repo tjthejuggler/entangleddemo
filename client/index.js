9ef8dfb5967e26846d479f31bed7d8d2381161f1
tjthejuggler <Github183630>
2019-03-12 14:43:20 -0500

    making P key give player count

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 import { WINDOW_WIDTH, WINDOW_HEIGHT } from './config'
 import Game from './state/Game'
-
+var globalPlayerCount = 8
 class App extends Phaser.Game {
   constructor () {
-  	var globalPlayerCount = 8
+  	
     super(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO)
     this.state.add('Game', Game)
     this.state.start('Game')
