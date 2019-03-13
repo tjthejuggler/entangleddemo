189a128903a81f5e4d8c06f5c64d3cfb21f1bc1a
tjthejuggler <Github183630>
2019-03-13 14:17:14 -0500

    gutting the car aspect of it, making it into a list

--- client/index.js
+++ client/index.js
@@ -1,34 +1,42 @@
 import { WINDOW_WIDTH, WINDOW_HEIGHT } from './config'
 import Game from './state/Game'
 import player from './state/player/index'
-class App extends Phaser.Game {
-  constructor () {
+
+class myClass {
+constructor () {
+	console.log("myClass!")
+}
+
+}
+
+// class App extends Phaser.Game {
+//   constructor () {
 
 
-    super(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO)
-    this.state.add('Game', Game)
-    this.state.start('Game')
+//     super(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO)
+//     this.state.add('Game', Game)
+//     this.state.start('Game')
 
-		// var clickMeButton = document.createElement('button');
-  //   clickMeButton.id = 'myButton';
-  //   clickMeButton.innerHTML = 'Click Me';
-  //   clickMeButton.style.background = '#4FFF8F';
-  //   clickMeButton.style.width = '200px'; // setting the width to 200px
-  //   clickMeButton.style.height = '200px'; // setting the height to 200px
-  //   clickMeButton.style.zIndex = 1000;
-  //   clickMeButton.onclick = function(){
-  //       if(this.player.myVariable == 0){
-  //           this.player.myVariable = 1
-  //       }else{
-  //           this.player.myVariable = 0
-  //       }
-  //       console.log("Button clicked!")
-  //       alert("Button clicked!")
-  //   }
-  //   document.body.appendChild(clickMeButton);
+// 		// var clickMeButton = document.createElement('button');
+//   //   clickMeButton.id = 'myButton';
+//   //   clickMeButton.innerHTML = 'Click Me';
+//   //   clickMeButton.style.background = '#4FFF8F';
+//   //   clickMeButton.style.width = '200px'; // setting the width to 200px
+//   //   clickMeButton.style.height = '200px'; // setting the height to 200px
+//   //   clickMeButton.style.zIndex = 1000;
+//   //   clickMeButton.onclick = function(){
+//   //       if(this.player.myVariable == 0){
+//   //           this.player.myVariable = 1
+//   //       }else{
+//   //           this.player.myVariable = 0
+//   //       }
+//   //       console.log("Button clicked!")
+//   //       alert("Button clicked!")
+//   //   }
+//   //   document.body.appendChild(clickMeButton);
 	
-  // }
-}
-}
+//   // }
+// }
+// }
 
-const SimpleGame = new App()
+// const SimpleGame = new App()
