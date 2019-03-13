94af9cb3497696d822d0ecf965dc80cedcb9fb84
tjthejuggler <Github183630>
2019-03-12 22:49:51 -0500

    making P give the player count

--- client/index.js
+++ client/index.js
@@ -6,6 +6,11 @@ class App extends Phaser.Game {
     super(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO)
     this.state.add('Game', Game)
     this.state.start('Game')
+    var clickMeButton = document.createElement('button');
+	clickMeButton.id = 'myButton';
+	clickMeButton.innerHTML = 'Click Me';
+	clickMeButton.style.background = '#4FFF8F';
+	document.body.appendChild(clickMeButton);
 	
   }
 }
