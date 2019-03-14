35e3cc86082b72aa5a6cc9668b00b3ddd378c576
tjthejuggler <Github183630>
2019-03-14 01:30:13 -0500

    gutting the car aspect of it, making it into a list

--- client/state/sockets/getOtherPlayerInfo.js
+++ client/state/sockets/getOtherPlayerInfo.js
@@ -5,7 +5,9 @@
 const getOtherPlayerInfo = (socket, myName, otherPlayer, theParticle) => {
   socket.on('update-players', function(data) {
       var playersData = data.playersVar
+      console.log("playersData", playersData)
       var theParticleFromServer = data.theParticleVar 
+      console.log("theParticleFromServer", theParticleFromServer)
     // Iterate over all players
     for (let index in playersData) {
       const data = playersData[index]
