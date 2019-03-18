f60e292933860df3d92c9961c07ca736ef564744
Timothy Ross <tjthejuggler@gmail.com>
2019-03-17 21:59:16 -0500

    trying to add radiobuttons

--- client/index.js
+++ client/index.js
@@ -266,7 +266,7 @@ radioInput.innerHTML = 'first';
   var radioInputII = document.createElement('input');
 radioInputII.setAttribute('type', 'radio');
 radioInputII.setAttribute('name', 'testing');//name is defining the group, different name, different group
-radioInput.setAttribute('value', 'second value');
+radioInputII.setAttribute('value', 'second value');
 radioInputII.innerHTML = 'second';
 
   // let coordsII = radioInput.getBoundingClientRect();
