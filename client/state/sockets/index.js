535f891ed17ce63f15f3c8abc014a1e5ee8c0b43
Timothy Ross <tjthejuggler@gmail.com>
2019-03-18 19:59:55 -0500

    trying to add radiobuttons

--- client/state/sockets/index.js
+++ client/state/sockets/index.js
@@ -7,3 +7,25 @@ export function getOtherPlayersCount()  {
 	}
     return count+100
 }
+
+const getOtherPlayerInfo = (socket, myName, otherPlayer, theParticle) => {
+  socket.on('update-players', function(data) {
+      var playersData = data.playersVar
+      //console.log("playersData", playersData)
+      var theParticleFromServer = data.theParticleVar 
+      //console.log("theParticleFromServer", theParticleFromServer)
+    // Iterate over all players
+    for (let index in playersData) {
+      const data = playersData[index]
+
+      // Update otherPlayer data
+      if (data.playerName !== myName) {
+        otherPlayer.playerName = data.playerName
+
+      }
+    }
+  theParticle.isSetTo = theParticleFromServer
+  })
+}
+
+export default getOtherPlayerInfo
