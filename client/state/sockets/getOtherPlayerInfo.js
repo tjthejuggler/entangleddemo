8ba3c5e808df9fb8bb8ce2bd743a878dfb015e22
Timothy Ross <tjthejuggler@gmail.com>
2019-03-25 11:59:27 -0500

    making labs able to have their own particle

--- client/state/sockets/getOtherPlayerInfo.js
+++ client/state/sockets/getOtherPlayerInfo.js
@@ -6,18 +6,18 @@ const getOtherPlayerInfo = (socket, myName, myLabName, otherPlayer, theParticle)
       //console.log("theParticleFromServer", theParticleFromServer)
     // Iterate over all players
     for (let index in playersData) {
-      const data = playersData[index]
+      const datas = playersData[index]
 
 //TODO make it so it only shows other player name if in the same lab
 
        //console.log('myLabName',myLabName)
        //console.log('data.playerLabName',data.playerLabName)
       // Update otherPlayer data
-      if (data.playerLabName == myLabName && data.playerName !== myName) {
+      if (datas.playerLabName == myLabName && datas.playerName !== myName) {
         //TODO, USE SOME CONSOLE PRINTS TO FIGURE OUT WHY THE ABOVE LINE LEAVES BOTH PLAYERS AS PLAYER 3
               //if (data.playerName !== myName) {
-        otherPlayer.playerName = data.playerName
-        otherPlayer.playerLabName = data.playerLabName
+        otherPlayer.playerName = datas.playerName
+        otherPlayer.playerLabName = datas.playerLabName
         theParticle.isSetTo = theParticleFromServer
       }
     }
